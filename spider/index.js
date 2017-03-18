ca915eb9da358270d93eff040b1c8360b332685d
arron <191446367@qq.com>
2017-03-18 09:25:25 +0800

    add nodejs spider

---
+++ spider/index.js
@@ -0,0 +1,44 @@
+const request = require('request');
+const cheerio = require('cheerio');
+const fs = require('fs');
+
+const queryUrl = 'http://www.mmjpg.com/home/';
+const chooseDom = ".pic ul li a img";
+const pagemax = 10;		// 获取到多少页的内容
+const startindex = 1;
+const dir = './source'
+
+var urls = [];
+var sumConut = 0;
+
+function getHtml(url, page) {
+  console.log(`正在获取第${page}页的图片>>>>>`);
+  //发送请求
+	request(url+page, function(error, response, body) {
+		if(!error && response.statusCode == 200) {
+			var $ = cheerio.load(body);
+			$(chooseDom).each(function() {
+				var src = $(this).attr('src');
+				console.log('正在下载' + src);
+				download(src, dir, Math.floor(Math.random()*100000) + src.substr(-4,4));
+				console.log('下载完成');
+			});
+		}
+  });
+  if(page < pagemax) {
+    page = page + 1;
+    getHtml(url, page)
+  }
+}
+
+function download(imgUrl,dir,filename) {
+  request.head(imgUrl, function(err, res, body){
+    request(imgUrl).pipe(fs.createWriteStream(dir + "/" + filename));
+  });
+}
+
+function start() {
+	getHtml(queryUrl, startindex);
+}
+
+start();
